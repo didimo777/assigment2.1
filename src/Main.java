26db59a8d2ad2d9ebaab29f21a9305a4ea26ac4d
didimo777 <momenzanovadilnaz@gmail.com>
2026-01-04 02:55:02 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        Vehicle car = new Car("Toyota Camry", 2018, 20000, 4);
+        Vehicle bus = new Bus("Mercedes Bus", 2015, 50000, 50);
+
+        System.out.println(car);
+        System.out.println("Insurance fee: " + car.calculateInsuranceFee());
+
+        System.out.println();
+
+        System.out.println(bus);
+        System.out.println("Insurance fee: " + bus.calculateInsuranceFee());
+    }
 }
